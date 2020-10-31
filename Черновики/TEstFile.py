29ed57a49e76adb08b09cacf7a39c9e1e9268e52
Weakne55 <karpovich.bv@phystech.edu>
2020-10-31 11:51:55 +0300

    #

--- Черновики/TEstFile.py
+++ Черновики/TEstFile.py
@@ -1,12 +1,14 @@
-import turtle as t
-
-t.shape('turtle')
-
-with open('input.txt') as file:
-    for line in file:
-         print('line: "', line, '"')
-
-
+def MathOp():
+    classic_division = 3 / 2  ## Calculate 3/2 here
+    floor_division = 5 // 2  ## Calculate 3//2 here
+    modulus = 1 % 2  ## Calculate 3%2 here
+    power = 7 ** 2  ## Calculate 3**2 here
+    ## Returning the calculations for evaluation
+    return [classic_division, floor_division, modulus, power]
 
 
-
+[classic_division, floor_division, modulus, power] = MathOp()
+print(classic_division)
+print(floor_division)
+print(modulus)
+print(power)
